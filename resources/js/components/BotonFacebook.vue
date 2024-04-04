61784c6c8a3ff4dc5512136d0e823c464e575003
root <jesus_cova88@hotmail.com>
2024-04-04 20:51:57 -0300

    Inicial

---
+++ resources/js/components/BotonFacebook.vue
@@ -0,0 +1,21 @@
+<template>
+    <button @click="openWhatsAppApp">Abrir WhatsApp 2</button>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      openWhatsAppApp() {
+        var destination = "twitter://"; 
+        if( navigator.userAgent.match(/Android/i) ) {
+            // use Android's redirect
+            document.location = destination;   
+        }   
+        else {
+            // use iOS redirect
+            window.location.replace( destination );
+        }
+      }
+    }
+  }
+  </script>
